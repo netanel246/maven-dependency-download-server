47cbd5ef9ba20f200c452edc67d8e175136350b6
fikavexik <fikavexik@2ether.net>
2018-03-20 14:19:49 +0200

    generic script

--- copy-only-new-files.py
+++ copy-only-new-files.py
@@ -2,59 +2,45 @@ import sys
 import os
 import subprocess
 import shutil
-from colorama import init
-init()
-from colorama import Fore, Back, Style
 import uuid
 
 def main(argv):
-	initialize_internals()
+	internals_path = initialize_internals()
+	internals_m2 = os.path.join(internals_path, '.m2')
 	
-	initialize_users_dependenies_dir(argv[0])
-def run():
-	dependencies_path = "C:\\Users\\6\\Desktop\\maven-imports\\jersey\\m2"
-	maven_base_libs_path = "C:\\Users\\6\\Desktop\\Maven-imports\\base-maven-libs"
-	output_path = "C:\\Users\\6\\Desktop\\maven-imports\\jersey\\dependencies"
-
+	temp_path = initialize_users_dependenies_dir(internals_path, argv[0])
+	temp_m2 = os.path.join(temp_path, '.m2')
+	
+	run(internals_m2, temp_m2, argv[1])
+	
+def run(maven_base_libs_path, dependencies_path, output_path):
 	dependencies_with_base_libs = []
 	for root, directories, filenames in os.walk(dependencies_path):
 		for filename in filenames: 
-			dependencies_with_base_libs.append(os.path.join(root.replace(dependencies_path, ''), filename))
+			dependencies_with_base_libs.append(os.path.join(root.replace(dependencies_path + '\\', ''), filename))
 			
 
 	maven_base_libs = []
 	for root, directories, filenames in os.walk(maven_base_libs_path):
 		for filename in filenames: 
-			maven_base_libs.append(os.path.join(root.replace(maven_base_libs_path, ''), filename))
+			maven_base_libs.append(os.path.join(root.replace(maven_base_libs_path + '\\', ''), filename))
 
 	dependencies = [lib for lib in dependencies_with_base_libs if lib not in maven_base_libs]
 
-	if os.path.exists(output_path + '\\tree'):
-		shutil.rmtree(output_path + '\\tree')
-	if os.path.exists(output_path + '\\files'):	
-		shutil.rmtree(output_path + '\\files')
+	output_tree = os.path.join(output_path, 'tree-'+str(uuid.uuid4())[:8])
+	print('writing to ' + output_tree)
 
 	for dependency in dependencies:
 		dir, filename = os.path.split(dependency)
 		
-		input_file = dependencies_path + dependency
-		output_tree_dir = output_path +'\\tree'+ dir
-		output_files_dir = output_path + '\\files'
+		input_file = os.path.join(dependencies_path, dependency)
+		
+		output_tree_dir = os.path.join(output_tree, dir)
 		
 		if not os.path.exists(output_tree_dir):
 			os.makedirs(output_tree_dir)	
-			
-		shutil.copy(input_file, output_tree_dir)
 		
-		if not os.path.exists(output_files_dir):
-			os.makedirs(output_files_dir)
-		
-		if os.path.exists(output_files_dir + '\\' + filename):
-			print dir + '\\'
-			print Fore.RED + filename
-			print(Style.RESET_ALL) + ' already exists!'
-
-		shutil.copy(input_file, output_files_dir)
+		shutil.copy2(input_file, output_tree_dir)
 
 	print 'Copied {} files to dependencies directory'.format(len(dependencies))
 
@@ -64,11 +50,14 @@ def initialize_internals():
 	
 	settings_path = create_mvn_settings(internals_path)
 	
-	subprocess.Popen(r'mvn dependency:go-offline -s {}'.format(settings_path), cwd=internals_path, shell=True)
+	print('importing missing base dependencies')
+	output = subprocess.check_output(r'mvn dependency:go-offline -s {}'.format(settings_path), cwd=internals_path, shell=True)
+	print(output)
+	
+	return internals_path
 
-def initialize_users_dependenies_dir(users_pom_path):
-	dirname = os.path.dirname(os.path.realpath(__file__))
-	temp_dir = os.path.join(dirname, str(uuid.uuid4()))
+def initialize_users_dependenies_dir(internals_path, users_pom_path):
+	temp_dir = os.path.join(internals_path, str(uuid.uuid4()))
 	os.makedirs(temp_dir)
 
 	temp_pom_path = os.path.join(temp_dir, 'pom.xml')
@@ -79,12 +68,15 @@ def initialize_users_dependenies_dir(users_pom_path):
 		with open(temp_pom_path, 'w') as temp_pom:
 			temp_pom.write(users_pom.read())
 	
-	subprocess.Popen(r'mvn dependency:go-offline -s {}'.format(temp_settings_path), cwd=temp_dir, shell=True)
-
+	print('copying base depedencies')
+	shutil.copytree(os.path.join(internals_path, '.m2'), os.path.join(temp_dir, '.m2'))
 	
-
-
-
+	print('downloading new dependencies')
+	output = subprocess.check_output(r'mvn dependency:go-offline -s {}'.format(temp_settings_path), cwd=temp_dir, shell=True)
+	print(output)
+	
+	return temp_dir
+	
 def create_mvn_settings(settings_directory, m2_directory=None):
 	if m2_directory is None:
 		m2_directory = settings_directory
