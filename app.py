3ea68d4ae82c2da9c29755c1ec3f4bda970fef82
liav tibi <liavt11@gmail.com>
2019-06-16 12:52:12 +0300

    Add client and server using flask Add route /about with example of how to use Add route /public_maven_dependency that can get the dependencies by

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, send_file, url_for, flash, redirect
 import process
+from os import path
+import uuid
+from utils.forms import MavenForm
+from utils.consts import ABOUT_PUBLIC_MAVEN_XML_EXAMPLE, DOWNLOADED_FILES_DIR, ZIP_EXTENSION
+import shutil
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "ffd4gf78th4ythh49j4s7498dwde4897ry89"
 
-@app.route('/copy_only_new_files', methods=['GET'])
-def copy_only_new_files(pom):
-    argv = [pom]
-    process.do_run(argv)
+
+@app.route('/', methods=['GET'])
+def home():
+    return redirect(url_for('public_maven_dependency'),)
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html', title='about', public_maven_xml_example=ABOUT_PUBLIC_MAVEN_XML_EXAMPLE)
+
+
+@app.route('/public_maven_dependency', methods=['GET', 'POST'])
+def public_maven_dependency():
+    form = MavenForm()
+    if form.validate_on_submit():
+        # try download the jars
+        try:
+            jars_zip_path = download_jars(form.maven_xml_pom.data)
+            return send_file(jars_zip_path, as_attachment=True, attachment_filename='jars.zip')
+        except Exception as e:
+            flash('Failed to download the jars', 'danger')
+    return render_template('public_maven_dependency.html', title='public maven', form=form)
+
+
+def download_jars(maven_jar_pom):
+    jars_dir_name = str(uuid.uuid4())
+    jars_dir_path = path.join(DOWNLOADED_FILES_DIR, jars_dir_name)
+    jars_zip_path = path.join(DOWNLOADED_FILES_DIR, jars_dir_name)
+    process.do_run(maven_jar_pom, jars_dir_path)
+    shutil.make_archive(jars_zip_path, ZIP_EXTENSION, jars_dir_path)
+    return jars_zip_path + f".{ZIP_EXTENSION}"
 
 
 if __name__ == '__main__':
